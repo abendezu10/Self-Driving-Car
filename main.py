c69c15be1e3f44586a4a945317d76f7b67f8497e
Alexander Bendezu <abendezu385@gmail.com>
2024-10-05 01:09:50 -0400

    added main python file

---
+++ main.py
@@ -0,0 +1,16 @@
+# main file where the while True: loop will run
+import motor_functions
+import cv2
+import 
+
+
+# I am thinking that we should have the tensorflow lite model on seperate file 
+
+
+
+
+
+while True:
+    # motor functions 
+    # opencv
+    # 
--- motor_functions.py
+++ motor_functions.py
@@ -2,6 +2,14 @@ import RPio.GPIO as GPIO
 import time
 
 
+input1 = 17
+input2 = 22
+input3 = 23
+input4 = 24
+enableA = 12
+enableB = 13
+
+
 # Initializes all GPIO Pins
 
 def init():
@@ -9,19 +17,15 @@ def init():
     GPIO.setmode(GPIO.BCM)
 
     # GPIO PIN 17
-    input1 = 17
     GPIO.setup(input1, GPIO.OUT)
 
     # GPIO PIN 22
-    input2 = 22
     GPIO.setup(input2, GPIO.OUT)
 
     # GPIO PIN 23
-    input3 = 23
     GPIO.setup(input3, GPIO.OUT)
 
     # GPIO PIN 24
-    input4 = 24
     GPIO.setup(input4, GPIO.OUT)
 
     # Controls speed and torque of motor
@@ -29,9 +33,49 @@ def init():
     GPIO.setup(enableA, GPIO.OUT)
 
     enableB = 13
-    GPIO.setup(enableB, GPIO.OUT)
-
+    GPIO.setup(enableB, GPIO.OUT) 
     
-    GPIO.output(enableA, GPIO.HIGH)
-    GPIO.output(enableB, GPIO.LOW)
+    pwm_enA = GPIO.PWM(enableA, 1000) 
+    pwm_enA.start(0)
 
+    pwm_enB = GPIO.PWM(enableA, 1000)
+    pwm_enB.start(0)
+
+
+def forward():
+    
+    # Forward motion because of H-Bridge component
+
+    GPIO.output(input1, GPIO.HIGH)
+
+    GPIO.output(input2, GPIO.LOW)
+
+    GPIO.output(input3, GPIO.HIGH)
+
+    GPIO.output(input4, GPIO.LOW)
+    
+    pwm_enA.ChangeDutyCycle(75)
+
+    pwm_enB.ChangeDutyCycle(75)
+    
+
+#def slow_forward():
+    
+
+
+
+def stop():
+    
+    # Turns off all motors
+
+    GPIO.output(input1, GPIO.LOW)
+
+    GPIO.output(input2, GPIO.LOW)
+
+    GPIO.output(input3, GPIO.LOW)
+
+    GPIO.output(input4, GPIO.LOW)
+    
+    pwm_enA.ChangeDutyCycle(0)
+
+    pwm_enB.ChangeDutyCycle(0)
