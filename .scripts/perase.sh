c29b43f9812b7feeb1dd9524a98192921ba48f27
Alexander Bendezu <abendezu385@gmail.com>
2024-10-25 20:58:38 -0400

    added sampling rate for pictures

---
+++ .scripts/perase.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# jpgfiles=$(ls -l ./pictures/*.jpg | wc -l)
+
+# echo $jpgfiles (should display number of jpg files)
+
+
+  
+  rm -f ./pictures/*.jpg 
+  echo "Files were deleted."
+  
+
+
--- camera.cpp
+++ camera.cpp
@@ -1,26 +1,62 @@
 #include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+#include <csignal>
+
+
 #include "opencv2/opencv.hpp"
 
 using namespace cv;
 using namespace std;
 
-int main () {
- 
+bool running = true;
+
+void signalHandler(int signum) {
+    cout << "\nInterrupt signal (" << signum << ") received. Exiting..." << endl;
+    running = false;
+}
+
+int main () 
+{
+  signal(SIGINT, signalHandler);
+
+
+  int img_count = 0; 
+  
+  string fname;
+
   VideoCapture cap(0);
 
   waitKey(1000);
 
-  Mat save_img; cap >> save_img;
+  Mat save_img; 
 
-  if(save_img.empty())
-  {
-    cerr << "Image was not captured!";
+ 
+
+  while(running){
+
+
+    fname = "pictures/image" + to_string(img_count) + ".jpg";
+    
+     if(img_count == 10){
+
+      int result = system("./.scripts/perase.sh");
+      cout<<"Script ran!"<<endl;
+
+    }
+
+
+    cap >> save_img;
+    imwrite(fname, save_img);
+    img_count++; 
+    
+    this_thread::sleep_for(chrono::seconds(2));
+    
 
   }
-  
-  imwrite("pictures/test.jpg", save_img);
-  cout<<"Hello World!";
-  
+
+  int result = system("./.scripts/perase.sh"); 
 
 
   return 0;
